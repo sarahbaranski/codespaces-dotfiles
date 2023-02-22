c998cd5de1becd6bbfc915727a3b0562686e3cf7
Sarah Baranski <sbaranski@litmus.com>
2023-02-22 15:16:35 -0600

    basic awscli install

--- setup.sh
+++ setup.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+mkdir ~/.tmp && cd $_
+
+# install aws-cli v2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
+  unzip awscliv2.zip && \
+  sudo ./aws/install
