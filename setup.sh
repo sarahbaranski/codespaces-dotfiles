b0f6cc2ccabeecb2c86c8643900cc17836cd6ee3
Sarah Baranski <sbaranski@litmus.com>
2023-02-22 15:17:39 -0600

    add confirmation

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ mkdir ~/.tmp && cd $_
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
   unzip awscliv2.zip && \
   sudo ./aws/install
+
+echo "AWS installed!"
