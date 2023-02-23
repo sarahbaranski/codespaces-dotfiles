04ac78f6dab681addffcd9194f1a712e295e938b
Sarah Baranski <sbaranski@litmus.com>
2023-02-23 13:12:49 -0600

    switched to rsync install

--- setup.sh
+++ setup.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-mkdir ~/.tmp && cd $_
+# install rsync
+sudo apt install rsync
 
-# install aws-cli v2
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
-  unzip awscliv2.zip && \
-  sudo ./aws/install
-
-echo "AWS installed!"
+echo "rsync installed!"
